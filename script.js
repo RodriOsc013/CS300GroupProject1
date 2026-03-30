41e8e35304f6ca48d80bd93274ecce975c2b913e
Oscar Rodriguez <orodriguezfc@gmail.com>
2026-03-29 19:13:16 -0500

    added theme changer to all pages, css and script

--- script.js
+++ script.js
@@ -34,5 +34,25 @@
     if (!isClickInside) {
       closeNav();
     }
+    const toggleBtn = document.getElementById("theme-toggle");
+
+if (toggleBtn) {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+        toggleBtn.textContent = "☀️";
+    }
+
+    toggleBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+            toggleBtn.textContent = "☀️";
+        } else {
+            localStorage.setItem("theme", "light");
+            toggleBtn.textContent = "🌙";
+        }
+    });
+}
   });
 })();
